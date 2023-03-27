019ef9a41f10429af7d22e83a38f31c8f43710ce
Alex Lalljee <alex.lalljee@gmail.com>
2023-03-27 17:31:08 +0200

    updated app.py code for dynamic columns and neighbours variable

--- website/prediction.py
+++ website/prediction.py
@@ -3,10 +3,11 @@ from matplotlib.pyplot import clf
 from potluck_code.logic import model
 
 
-def predict(ingredients):
-    clf = model.easy_search(ingredients, 3)
+def predict(ingredients, mix):
+    clf = model.easy_search(ingredients, mix)
     return clf
 
 # testing the predict function
 if __name__=='__main__':
-    print(predict(['water', 'salt', 'tomatoes']))
+    print(predict(['water', 'salt', 'tomatoes'], 3))
+    # print('Please enter some ingredients')
